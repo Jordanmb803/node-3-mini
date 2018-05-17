ae226f98c2e45167c47c9a58507f1029a2907251
Jordan Buonforte <jordan@Jordans-MacBook-Air.local>
2018-05-17 09:19:36 -0600

    comitt

--- server/index.js
+++ server/index.js
@@ -1,16 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mc = require( `./controllers/messages_controller` );
+const expressSession = require('express-session');
+const mc = require(`./controllers/messages_controller`);
+const createInitialSession = require('./middlewares/session');
+const filterBadWords = require('./middlewares/filter');
 
 const app = express();
 
-app.use( bodyParser.json() );
-app.use( express.static( `${__dirname}/../build` ) );
+app.use(bodyParser.json());
+app.use(express.static(`${__dirname}/../build`));
+app.use(expressSession({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized:true,
+    cookie:{
+        secure:false,
+        maxAge:1000*10
+    }
 
-app.post( "/api/messages", mc.create );
-app.get( "/api/messages", mc.read );
-app.put( "/api/messages", mc.update );
-app.delete( "/api/messages", mc.delete );
+}))
+app.use(createInitialSession);
 
-const port = process.env.PORT || 3000
-app.listen( port, () => { console.log(`Server listening on port ${port}.`); } );
+
+app.post("/api/messages", filterBadWords, mc.create);
+app.get("/api/messages", mc.read);
+app.put("/api/messages", filterBadWords, mc.update);
+app.delete("/api/messages", mc.delete);
+
+const port = process.env.PORT || 3565
+app.listen(port, () => { console.log(`Server listening on port ${port}.`); });
